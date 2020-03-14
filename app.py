62a096f2a87a35fe2ab1488b8482ac174ebca614
Joshua Kagawa <joshuakagawa@gmail.com>
2020-03-14 15:58:36 -0400

    Test commit

--- app.py
+++ app.py
@@ -511,12 +511,11 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
 
 # Or specify port manually:
-'''
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+    port = int(os.environ.get('PORT', 81))
+    app.run(host='127.0.0.1', port=port)
+    
